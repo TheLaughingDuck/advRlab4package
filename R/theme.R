0190454502d705037b91ec9bfede4c93ece5fb4e
Simon Jorstedt <jorstedtsimon@gmail.com>
2023-10-02 16:46:39 +0200

    Cleaned up theme.R
    I cleaned up the theme function, removed the library load, commented out problematic code. I put the test code in the unit test file for theme.

--- R/theme.R
+++ R/theme.R
@@ -6,38 +6,28 @@
 #'
 #' @return A `ggplot` object.
 #'
-#' @export
+#' @export theme
 
 # Create a custom function with a different name
 theme <- function(data) {
   # ---V--- CHECK INPUT ---V---
   # Check if the input is a ggplot object
-  #stopifnot(is.ggplot(plot))
+  stopifnot("argument \"plot\" is not ggplot object" = is.ggplot(plot))
   # ---^--- CHECK INPUT ---^---
 
   # Apply the ggplot2 theme
-  library(ggthemes)
+  #library(ggthemes)
 
   # Create a scatter plot with customized elements
-  plot <- ggplot(data = data, aes(x = !!sym("GPA"), y = !!sym("StudyHours"))) +
-    geom_point() +
-    labs(title = "Linköping University Student Data", subtitle = "GPA vs. Study Hours") +
-
-    theme_gdocs()
+  # plot <- ggplot(data = data, aes(x = !!sym("GPA"), y = !!sym("StudyHours"))) +
+  #   geom_point() +
+  #   labs(title = "Linköping University Student Data", subtitle = "GPA vs. Study Hours") +
+  #
+  #   theme_gdocs()
 
   # Return the modified plot
   return(plot)
 }
-
-# Example data (replace with  data)
-liu_data <- data.frame(
-  StudentID = 1:20,
-  GPA = c(3.2, 3.5, 3.8, 2.9, 3.6, 3.2, 3.9, 2.7, 3.4, 3.1, 3.7, 3.0, 3.5, 3.8, 2.8, 3.3, 3.6, 3.2, 3.9, 2.9),
-  StudyHours = c(20, 25, 30, 15, 28, 22, 32, 12, 27, 18, 29, 14, 26, 31, 16, 23, 28, 21, 33, 17)
-)
-
-# Create the custom scatter plot using the function
-theme(liu_data)
 
 
 
--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -15,3 +15,15 @@ test_that("for correct output format", {
 # })
 
 
+# PLOT TESTS
+# Example data
+liu_data <- data.frame(
+  StudentID = 1:20,
+  GPA = c(3.2, 3.5, 3.8, 2.9, 3.6, 3.2, 3.9, 2.7, 3.4, 3.1, 3.7, 3.0, 3.5, 3.8, 2.8, 3.3, 3.6, 3.2, 3.9, 2.9),
+  StudyHours = c(20, 25, 30, 15, 28, 22, 32, 12, 27, 18, 29, 14, 26, 31, 16, 23, 28, 21, 33, 17)
+)
+
+# Create the custom scatter plot using the function
+theme(liu_data)
+
+
